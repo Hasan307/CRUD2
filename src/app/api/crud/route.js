7dd2725763f370b97de19163e266dad94e45931b
Md Abu Hasan <m.a.hasan307@gmail.com>
2023-10-30 02:59:33 +0600

    update

--- src/app/api/crud/route.js
+++ src/app/api/crud/route.js
@@ -29,14 +29,36 @@ export async function POST (req, res) {
 
 
 
-        const read_product= await prisma.product.findMany({
-            where: {
-                id: 1,
-            },
-        })
+        // const read_product= await prisma.product.findMany({
+        //     where: {
+        //         id: 1,
+        //     },
+        // })
+
+    //     const result = await prisma.product.aggregate({
+    //         _avg: { price: true },
+    //         _count: { price: true },
+    //         _max: { price: true },
+    //         _min: { price: true },
+    //         _sum: { price: true },
+    //   });
+
+    const result = await prisma.product_review.groupBy({
+        by:["rating"],
+        _count:{
+            id:true
+             },
+             having: {
+                 rating: {
+                     lte: 4
+                 }
+             }
+      
+    })
+
        
         
-        return NextResponse.json({status: "ok"})
+        return NextResponse.json({status: "ok", result: result})
        
     }
     catch(e){
